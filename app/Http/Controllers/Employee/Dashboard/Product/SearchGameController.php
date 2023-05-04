d738de6a00de8c956cf542a3a19e21d85172147d
BE-S <BE-S@example.com>
2023-05-04 21:44:46 +0700

    Added adminLTE 3

---
+++ app/Http/Controllers/Employee/Dashboard/Product/SearchGameController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Employee\Dashboard\Product;
+
+use App\Http\Controllers\Controller;
+use App\Models\Employee\Market\Game;
+use Illuminate\Http\Request;
+
+class SearchGameController extends Controller
+{
+    public function search(Request $request)
+    {
+        $games = [];
+        foreach ($request->all() as $key => $category) {
+            $games = Game::where($key, $category)->whereNull('deleted_at')->get();
+        }
+        $viewLoad = view('Admin.Layouts.games', compact('games'));
+
+        return response()->json([
+            'viewLoad' => $viewLoad->toHtml(),
+        ]);
+    }
+}
